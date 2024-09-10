0961dd1beb5b6c55bb9c3a333954c3dfe415e4ef
ChenLuWei317 <2274300100@qq.com>
2024-09-10 11:48:25 +0800

    测试
    测试

---
+++ src/main/java/com/app/warehouse_management_system/controller/PrimaryStageController.java
@@ -0,0 +1,13 @@
+package com.app.warehouse_management_system.controller;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrimaryStageController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
