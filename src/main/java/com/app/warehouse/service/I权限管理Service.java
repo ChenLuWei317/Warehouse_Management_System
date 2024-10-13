2e1dc0bfec9799262731b124491a67355723be61
qqq <2642196117@qq.com>
2024-10-14 00:24:47 +0800

    首页

---
+++ src/main/java/com/app/warehouse/service/I权限管理Service.java
@@ -0,0 +1,16 @@
+package com.app.warehouse.service;
+
+import com.app.warehouse.model.权限管理;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 魏陈露
+ * @since 2024-10-10
+ */
+public interface I权限管理Service extends IService<权限管理> {
+
+}
